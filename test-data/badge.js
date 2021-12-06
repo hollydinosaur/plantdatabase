91f136b1e41e44232b6b2178b2c2669e67b3fa08
Holly <hollykgreaves@gmail.com>
2021-12-06 14:50:20 +0000

    adds badge data

---
+++ test-data/badge.js
@@ -0,0 +1,8 @@
+const badges = [
+	{ name: "1 week", img_url: "suhfjs.png" },
+	{ name: "2 week", img_url: "jshfjsh.jpg" },
+	{ name: "30 different plants", img_url: "msbjsg.png" },
+	{ name: "60 different plants", img_url: "984684.jpg" },
+	{ name: "5 seeds", img_url: "nsnfhs.jpg" },
+	{ name: "10 vegetables", img_url: "9484.jpg" },
+];
---
+++ test-data/userData.js
@@ -0,0 +1,44 @@
+const users = [
+	{
+		username: "georgia123",
+		name: "georgia",
+		currentWeek: [{ name: "banana" }, { name: "peas" }],
+		badges: [{ name: "1 week", img_url: "aighdfjagdha.jpg" }],
+		streak: { currentStreak: 1, highestStreak: 1 },
+	},
+	{
+		username: "holly34",
+		name: "holly",
+		currentWeek: [
+			{ name: "brussels sprouts" },
+			{ name: "pumpkin seeds" },
+			{ name: "peas" },
+			{ name: "squash" },
+			{ name: "quinoa" },
+			{ name: "kale" },
+			{ name: "green beans" },
+			{ name: "chia seeds" },
+			{ name: "orange" },
+			{ name: "tangerine" },
+			{ name: "carrot" },
+		],
+		badges: [
+			{ name: "1 week", img_url: "aighdfjagdha.jpg" },
+			{ name: "2 week", img_url: "jhajsbdjd.png" },
+		],
+		streak: { currentStreak: 1, highestStreak: 2 },
+		plants = [	{ name: "brussels sprouts" },
+		{ name: "pumpkin seeds" },
+		{ name: "peas" },
+		{ name: "squash" },
+		{ name: "quinoa" },
+		{ name: "kale" },
+		{ name: "green beans" },
+		{ name: "chia seeds" },
+		{ name: "orange" },
+		{ name: "tangerine" },
+		{ name: "carrot" },]
+	},
+];
+
+module.exports = users;

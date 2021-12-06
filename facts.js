0926ac5f99958eb83ca3b07addef3879a1f64d1e
Holly <hollykgreaves@gmail.com>
2021-12-06 14:12:21 +0000

    adds seperate file for data

---
+++ facts.js
@@ -0,0 +1,64 @@
+const facts = [
+	{
+		fact: "kale is full of antioxidants which can help fight disease and infection",
+	},
+	{
+		fact: "spinach can have a probiotic effect on your gut, helping friendly bacteria",
+	},
+	{
+		fact: "try putting berries on your cereal to boost your fruit consumption",
+	},
+	{
+		fact: "making soup is a quick and easy way of adding more vegetables to your diet",
+	},
+	{ fact: "berries are superfruits and are rich in antioxidants" },
+	{
+		fact: "try spiralising vegetables such as carrots or courgettes to replace pasta or noodles",
+	},
+	{
+		fact: "pineapple contains a protein-digesting enzyme which can assist with the digestion of protein-rich foods",
+	},
+	{
+		fact: "find out which vegetables are currently in season as they can be cheaper and easier to find",
+	},
+	{
+		fact: "put a reminder on your fridge to encourage yourself to eat more diversely",
+	},
+	{ fact: "if a banana is going black, save waste by using it in a smoothie" },
+	{ fact: "help yourself reach your goal by meal planning for the week" },
+	{
+		fact: "artichokes are particularly high in fibre so roasting them can help improve our friendly gut bacteria",
+	},
+	{
+		fact: "Garlic, onion, leeks, asparagus, and whole grains are packed with special prebiotic fibers which gut microbes love. ",
+	},
+	{
+		fact: "try to avoid or limit snacking, and aim to give your gut bugs time to rest overnight",
+	},
+	{
+		fact: "avoid highly processed foods, as they don't provide much fibre and nutrients for your gut",
+	},
+	{
+		fact: "your gut microbiome weighs about 2kg and is bigger than the average human brain",
+	},
+	{ fact: "replace wraps with lettuce leaves for a fun take on fajitas" },
+	{
+		fact: "Red fruits and vegetables supply lycopene, which protects the skin from sun damage",
+	},
+	{
+		fact: "orange fruits and vegetables are packed with beta-carotene, which the body converts to vitamin A for healthy skin",
+	},
+	{
+		fact: "yellow fruits and vegetables protect the eyes from damage and help to reduce the risk of developing cataracts.",
+	},
+	{ fact: "green fruits and vegetables are rich in energising chlorophyll" },
+	{
+		fact: "purple fruits and vegetables are a good source of protective anthocyanins, which are great anti-agers",
+	},
+	{
+		fact: "make a packed lunch as they are usually more nutritious than store bought sandwiches",
+	},
+	{
+		fact: "organise your fridge by use by date so you can make the most out of fresh produce",
+	},
+];

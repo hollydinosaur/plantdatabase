a0f0179347e44490d1a41e920f9b060dd756c270
Holly <hollykgreaves@gmail.com>
2021-12-07 08:35:13 +0000

    adds correct arrays to index file

--- seed.js
+++ seed.js
@@ -1,49 +1,49 @@
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/mydb";
 const {
-  vegetables,
-  grains,
-  seeds,
-  nuts,
-  herbsAndSpices,
-  fruits,
+	vegetables,
+	grains,
+	seeds,
+	nuts,
+	herbsAndSpices,
+	fruits,
 } = require("./test-data/plantData");
 
 MongoClient.connect(url)
-  .then((database) => {
-    const dbo = database.db("mydb");
-    return dbo.createCollection("vegetables");
-  })
-  .then(() => {
-    return dbo.createCollection("grains");
-  })
-  .then(() => {
-    return dbo.createCollection("nuts");
-  })
-  .then(() => {
-    return dbo.createCollection("seeds");
-  })
-  .then(() => {
-    return dbo.createCollection("herbsAndSpices");
-  })
-  .then(() => {
-    return dbo.createCollection("fruits");
-  })
-  .then(() => {
-    return dbo.collection("vegetables").insertMany(vegetables);
-  })
-  .then(() => {
-    return dbo.collection("grains").insertMany(vegetables);
-  })
-  .then(() => {
-    return dbo.collection("nuts").insertMany(vegetables);
-  })
-  .then(() => {
-    return dbo.collection("seeds").insertMany(vegetables);
-  })
-  .then(() => {
-    return dbo.collection("herbsAndSpices").insertMany(vegetables);
-  })
-  .then(() => {
-    return dbo.collection("fruits").insertMany(vegetables);
-  });
+	.then((database) => {
+		const dbo = database.db("mydb");
+		return dbo.createCollection("vegetables");
+	})
+	.then(() => {
+		return dbo.createCollection("grains");
+	})
+	.then(() => {
+		return dbo.createCollection("nuts");
+	})
+	.then(() => {
+		return dbo.createCollection("seeds");
+	})
+	.then(() => {
+		return dbo.createCollection("herbsAndSpices");
+	})
+	.then(() => {
+		return dbo.createCollection("fruits");
+	})
+	.then(() => {
+		return dbo.collection("vegetables").insertMany(vegetables);
+	})
+	.then(() => {
+		return dbo.collection("grains").insertMany(grains);
+	})
+	.then(() => {
+		return dbo.collection("nuts").insertMany(nuts);
+	})
+	.then(() => {
+		return dbo.collection("seeds").insertMany(seeds);
+	})
+	.then(() => {
+		return dbo.collection("herbsAndSpices").insertMany(herbsAndSpices);
+	})
+	.then(() => {
+		return dbo.collection("fruits").insertMany(fruits);
+	});

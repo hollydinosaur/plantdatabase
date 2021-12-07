26007114d78dff9dc72953107cebd52701731a16
Holly <hollykgreaves@gmail.com>
2021-12-07 14:51:43 +0000

    adds folders

---
+++ seed/setupDbs.js
@@ -0,0 +1,11 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url)
+	.then((database) => {
+		const db = database.db("test");
+		db.createDatabase("plantAppDb_dev");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
